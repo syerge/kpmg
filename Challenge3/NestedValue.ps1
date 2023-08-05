7270f575a20fcba78d734b73be58cb6df937db71
Shrikant Yerge <shrikant.yerge@hotmail.com>
2023-08-05 16:07:20 +0000

    Challange 3 files

---
+++ Challenge3/NestedValue.ps1
@@ -0,0 +1,22 @@
+﻿function Get-NestedValue {
+    param (
+        [Parameter(Mandatory=$true)]
+        [Hashtable]$Object,
+        
+        [Parameter(Mandatory=$true)]
+        [string]$Key
+    )
+
+    $keyList = $Key -split '/'
+    $currentData = $Object
+
+    foreach ($key in $keyList) {
+        if ($currentData.ContainsKey($key)) {
+            $currentData = $currentData[$key]
+        } else {
+            return $null  # Key not found
+        }
+    }
+
+    return $currentData
+}
